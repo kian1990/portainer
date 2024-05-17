5b4ed9c720d6314bdb8c5ec5dc70230443e711ea
kian1990 <269590936@qq.com>
2024-05-17 10:00:28 +0800

    Add files via upload

---
+++ portainer-ee.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker stop portainer
+docker rm portainer
+docker volume rm portainer_data
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:2.20.2
