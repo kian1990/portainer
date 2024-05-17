043da1da917dcd3cade4607aef7927dd0f4b483d
kian1990 <269590936@qq.com>
2024-05-17 09:29:41 +0800

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 docker stop jenkins
 docker rm jenkins
-docker rmi jenkins
 docker volume rm jenkins_data
 docker volume create jenkins_data
 docker run -d -p 8080:8080 -p 50000:50000 --name jenkins --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v jenkins_data:/data jenkins/jenkins:latest
