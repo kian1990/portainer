00f9db340c175bbdf1a62bf97347e771f3f66358
kian1990 <269590936@qq.com>
2024-05-17 09:31:01 +0800

    Update rabbitmq.sh

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 docker stop rabbitmq
 docker rm rabbitmq
-docker rmi rabbitmq
 docker volume rm rabbitmq_data
 docker volume create rabbitmq_data
 docker run -d -p 15672:15672 --name rabbitmq --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v rabbitmq_data:/data rabbitmq:management
