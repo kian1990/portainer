b0284958d06765ce8e64f2fea0193f7dd5cabc92
kian1990 <269590936@qq.com>
2024-05-17 15:22:27 +0800

    Delete alpine-jdk21 directory

--- alpine-jdk21/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker stop alpine-jdk21
-docker rm alpine-jdk21
-docker volume rm alpine-jdk21_data
-docker volume create alpine-jdk21_data
-docker run -d -p 1001:8080 --name alpine-jdk21 --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v alpine-jdk21_data:/data alpine-jdk21:latest
