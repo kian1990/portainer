0852a58812110e9e350fa093bdb31a3dd830ed3e
kian1990 <269590936@qq.com>
2024-05-17 09:29:54 +0800

    Update kafka.sh

--- kafka.sh
+++ kafka.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 docker stop kafka
 docker rm kafka
-docker rmi kafka
 docker volume rm kafka_data
 docker volume create kafka_data
 docker run -d -p 9092:9092 --name kafka --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v kafka_data:/data apache/kafka:latest
