003123b5446804450bef1c7c800453feebb24f0e
kian1990 <269590936@qq.com>
2024-05-17 09:59:49 +0800

    Delete portainer.sh

--- portainer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#docker stop portainer
-#docker rm portainer
-#docker rmi portainer/portainer-ce
-#docker volume rm portainer_data
-docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
