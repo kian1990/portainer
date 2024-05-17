261ca0bbe9dc25dd679f618a16a038b3c43ee21f
kian1990 <269590936@qq.com>
2024-05-17 09:31:11 +0800

    Update redis.sh

--- redis.sh
+++ redis.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 docker stop redis
 docker rm redis
-docker rmi redis
 docker volume rm redis_data
 docker volume create redis_data
 docker run -d -p 6379:6379 --name redis --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v redis_data:/data redis:latest
