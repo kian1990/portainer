5a81fb99c35d1e679a79f8cff242fc8bebb24b5d
kian1990 <269590936@qq.com>
2024-08-08 16:41:16 +0800

    Update docker-easyconnect.sh

--- docker-easyconnect.sh
+++ docker-easyconnect.sh
@@ -3,4 +3,4 @@ docker stop docker-easyconnect
 docker rm docker-easyconnect
 docker volume rm docker-easyconnect_data
 docker volume create docker-easyconnect_data
-docker run -d --device /dev/net/tun --cap-add NET_ADMIN -ti -e PASSWORD=root -e URLWIN=1 --name docker-easyconnect --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/.ecdata:/root -p 5900:5901 -p 1080:1080 -p 8888:8888 -p 54530:54530 hagb/docker-easyconnect:7.6.7
+docker run -d --device /dev/net/tun --cap-add NET_ADMIN -ti -e DISABLE_PKG_VERSION_XML=1 -e PASSWORD=root -e URLWIN=1 --name docker-easyconnect --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/.ecdata:/root -p 5900:5901 -p 1080:1080 -p 8888:8888 -p 54530:54530 hagb/docker-easyconnect:7.6.7
